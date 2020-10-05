0f38aee6c357337dcd1fc74e28722257f3645f4c
unknown <hetland.tyler7@gmail.com>
2020-10-04 22:33:55 -0400

    adding file merge

--- reddit.py
+++ reddit.py
@@ -3,11 +3,14 @@
 Created on Thu Apr 12 18:45:40 2018
 
 @author: Image17
+
+
+https://www.reddit.com/api/v1/authorize?client_id=kkxH8GHYYVYx5Q&response_type=code&
+    state='henlo'&redirect_uri=http://127.0.0.1:65010/authorize_callback&duration=temporary&scope=read
 """
 
 import sys
 import praw
-import config
 import csv
 import glob
 
@@ -15,11 +18,11 @@ import glob
 Login to reddit through praw to provide a session for the scraper
 """
 def login():
-    session = praw.Reddit(username = config.username,
-                password = config.password,
-                client_id = config.client_id,
-                client_secret = config.client_secret,
-                user_agent = config.user_agent)
+    session = praw.Reddit(username = 'dr_wormhat',
+                password = '8@sACW2R7Sep4!u',
+                client_id = 'kkxH8GHYYVYx5Q',
+                client_secret =     'mRRC7fYfT0GJ_dlbPEFYEvJpo6k',
+                user_agent = 'suh dud dr_wormhat')
     return session
     
 """
@@ -29,21 +32,23 @@ def scrape(s):
     comments=[]
     r = login()
     for comment in r.subreddit(s).comments(limit=10000):
-        cmmt = []
-        # attain variables from prraw comment
-        create_timestamp = str(comment.created_utc)
-        author_name = comment.author.name
-        # when accessing the comment body from the csv you must decode with utf8
-        comment_body = str(comment.body.encode('utf8'))
-        # clean each piece of data individually if necessary
-        
-        # add to list to be written to a file
-        cmmt.append(create_timestamp)
-        cmmt.append(author_name)
-        cmmt.append(comment_body)
-        comments.append(cmmt)
+        if comment and comment.author:
+            cmmt = []
+            # attain variables from prraw comment
+            create_timestamp = str(comment.created_utc)
+            author_name = comment.author.name
+            # when accessing the comment body from the csv you must decode with utf8
+            comment_body = str(comment.body)
+            # clean each piece of data individually if necessary
+            
+            # add to list to be written to a file
+            cmmt.append(create_timestamp)
+            cmmt.append(s)
+            cmmt.append(author_name)
+            cmmt.append(comment_body)
+            comments.append(cmmt)
     # write list to file
-    with open('reddit/reddit-data-'+s+'.csv', mode='w', newline='') as reddit_data:
+    with open('reddit/reddit-data-'+s+'.csv', mode='w+', newline='',encoding='utf-8') as reddit_data:
         csv_writer = csv.writer(reddit_data, delimiter='|')
         for c in comments:
             csv_writer.writerow(c)
@@ -55,11 +60,12 @@ may have been retrieved twice.  Adjust the scheduling of the scraping to limit
 duplicates.
 """         
 def clean_duplicates():
+    print('cleaning...')
     files = glob.glob("reddit/*.*")
     for file in files:
         print('cleaning '+file)
         comments=[]
-        with open(file, newline='', encoding='UTF-8-sig') as csvfile:
+        with open(file, newline='', encoding='utf-8') as csvfile:
             spamreader = csv.reader(csvfile, delimiter='|')
             for row in spamreader:
                 comment=[]
@@ -69,11 +75,36 @@ def clean_duplicates():
                     comments.append(comment)
         print(len(comments))
         
-        with open (file, 'w', newline='') as csvfile:
+        with open (file, 'w', newline='',encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile, delimiter='|')
             csvfile.truncate()
             for c in comments:
                 csv_writer.writerow(c)
+    print('clean!')  
+
+
+def merge_files():
+    print('merging..')
+    files = glob.glob("reddit/*.*")
+    for file in files:
+        print('handling',file)
+        comments=[]
+        with open(file, newline='',encoding='utf-8') as infile:
+            reader = csv.reader(infile, delimiter='|')
+            for row in reader:
+                comment=[]
+                if row not in comments:
+                    for i in row:
+                        comment.append(i)
+                    comments.append(comment)
+        with open ('reddit/merged.csv', 'a', newline='',encoding='utf-8') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter='|')
+            csvfile.truncate()
+            for c in comments:
+                #c = c.append(file.split('-')[3].split('.')[0])
+                csv_writer.writerow(c)
+    print('merged!')
+
 
 """
 Takes command line arguments of subreddit names, uses praw api to retrieve ~1000
@@ -86,6 +117,9 @@ def main():
         scrape(subreddit)
         print('scraping ' + subreddit + ' completed!')
     print('completed')
+
+    merge_files()
+    clean_duplicates()
 
 if __name__ == '__main__':
     main()
